79fd49936d5cc4df7de8f2d15866bf7eac687bd6
유미령 <kioku714@gmail.com>
2018-10-18 17:57:41 +0900

    #5 Boilerplate 구성 중.
    - express와 vue.js 환경설정.
    - backend 기동: #sky-cherry/backend$ DEBUG=backend:* npm start
    - frondend 기동: #sky-cherry/frontend$ npm run dev
    - http://localhost:8080/test에서 영화 목록이 나오면 성공.

---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="movies">
+    <h1>영화 목록</h1>
+    <div v-for="movie in movies" class="movie" :key="movie.id">
+      <img v-bind:src="movie.poster" class="poster" fluid>
+      <div>
+        <router-link :to="{ name: 'detail', params: { id: movie.id }}">
+          <strong>{{ movie.name }}</strong>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    this.$http.get('/api/test')
+      .then((response) => {
+        this.movies = response.data
+      })
+  },
+  data () {
+    return {
+      movies: []
+    }
+  }
+}
+</script>
