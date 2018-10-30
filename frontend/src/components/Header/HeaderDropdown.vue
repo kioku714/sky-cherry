05b5529ee17507e8499960dcbd02c69b670c08dd
유미령 <kioku714@gmail.com>
2018-10-31 03:09:05 +0900

    @media(max-width: 991.98px) 일 경우 나타나는 dropdown item에 페이지 링크 연결.

--- frontend/src/components/Header/HeaderDropdown.vue
+++ frontend/src/components/Header/HeaderDropdown.vue
@@ -6,20 +6,20 @@
         class="img-avatar" />
     </template>
     <template slot="dropdown">
-      <b-dropdown-item>
+      <b-dropdown-item :to="{name: '질문하기', query: { sort: 'like' }}">
         <i class="fa fa-search" /> 질문검색
       </b-dropdown-item>
-      <b-dropdown-item>
+      <b-dropdown-item :to="{name: '질문 등록'}">
         <i class="fa fa-quora" /> 질문하기
       </b-dropdown-item>
-      <b-dropdown-item>
+      <b-dropdown-item :to="{name: 'Cherry Transfer'}">
         <i class="fa fa-bitcoin" /> Cherry Transfer
       </b-dropdown-item>
-      <b-dropdown-item>
+      <b-dropdown-item :to="{name: '프로필', query: { tab: 'notification' }}">
         <i class="fa fa-bell-o" /> 알림
         <!-- <b-badge variant="info">10</b-badge> -->
       </b-dropdown-item>
-      <b-dropdown-item>
+      <b-dropdown-item :to="{name: '프로필', query: { tab: 'info' }}">
         <i class="fa fa-user-o" /> 프로필
       </b-dropdown-item>
     </template>
