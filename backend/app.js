e5c0b8f10ce33626fb016b7640af175713e0a3cc
유미령 <kioku714@gmail.com>
2018-10-30 17:10:36 +0900

    #42 회원 로그인 로그인 화면에서 에러 메세지가 제대로 출력되지 않고 undefined가 나오는 이슈 해결. error handler에서 error.jade로 렌더링되어 발생.

--- backend/app.js
+++ backend/app.js
@@ -49,14 +49,11 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use((err, req, res, next) => 
+  res.status(err.status).json({
+    message: err.isPublic ? err.message : httpStatus[err.status],
+    // stack: config.env === 'development' ? err.stack : {}
+  })
+);
 
 module.exports = app;
