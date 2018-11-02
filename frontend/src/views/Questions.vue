4e50293997154e94b6aa369b8175eb3ea20002b6
chihwan <clghks@gmail.com>
2018-11-02 21:50:38 +0900

    오타 수정

--- frontend/src/views/Questions.vue
+++ frontend/src/views/Questions.vue
@@ -7,7 +7,7 @@
         <div class="content">
           <!-- 사용자 닉네임, 질문 생성 시간  -->
           <span class="question-owner">
-            <a class="username-link" v-bind:href="'/profile/' + question.createdBy._id">{{ question.createdBy.name }}</a>
+            <a class="username-link" v-bind:href="'/profiles/' + question.createdBy._id">{{ question.createdBy.name }}</a>
             {{ $moment.utc(question.createdAt).local().fromNow() }}
           </span>
           <!-- 질문 제목 -->
