65c5452f0464c73cdd2434d9661f408b7c41f305
chihwan <clghks@gmail.com>
2018-11-02 22:19:51 +0900

    질문 작성 디자인 수정

--- frontend/src/views/NewQuestion.vue
+++ frontend/src/views/NewQuestion.vue
@@ -1,102 +1,97 @@
 <template>
   <div class="animated fadeIn">
     <div class="col-md-12">
-      <div class="card">
-        <div class="card-header">
-          <h1>질문 등록</h1>
-        </div>
-        <div class="card-body">
-          <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
-            <div class="form-group row">
-              <label class="col-md-2 col-form-label">분야 : </label>
-              <div class="col-md-2">
-                <b-form-select class="form-control" v-model="form.mainField">
-                  <option v-for="option in $store.state.fieldItems" v-bind:value="option.mainFieldValue" :key="option.mainFieldValue">
-                    {{ option.mainFieldName }}
-                  </option>
-                </b-form-select>
-              </div>
-              <div class="col-md-2">
-                <b-form-select class="form-control" v-model="form.subField">
-                  <option v-for="option in getSubFieldItems(form.mainField)" v-bind:value="option.value" :key="option.value">
-                    {{ option.text }}
-                  </option>
-                </b-form-select>
+      <div class="card-body">
+        <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+          <div class="form-group row">
+            <label class="col-md-2 col-form-label">* 분야 : </label>
+            <div class="col-md-2">
+              <b-form-select class="form-control" v-model="form.mainField">
+                <option v-for="option in $store.state.fieldItems" v-bind:value="option.mainFieldValue" :key="option.mainFieldValue">
+                  {{ option.mainFieldName }}
+                </option>
+              </b-form-select>
+            </div>
+            <div class="col-md-2">
+              <b-form-select class="form-control" v-model="form.subField">
+                <option v-for="option in getSubFieldItems(form.mainField)" v-bind:value="option.value" :key="option.value">
+                  {{ option.text }}
+                </option>
+              </b-form-select>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-md-12 col-form-label" for="text-input"><h1>Ask a question...</h1></label>
+          </div>
+          <div class="from-group row">
+            <div class="col-md-12">
+              <b-form-input type="text" v-model="form.title" placeholder="제목을 입력하세요."></b-form-input>
+            </div>
+          </div>
+          <br>
+          <div class="form-group row">
+            <label class="col-md-2 col-form-label">성별/나이</label>
+            <label class="col-md-2 col-form-label">{{ "male" === profile.gender ? "남" : "여"}} / {{ form.age }}</label>
+            <label class="col-md-2 col-form-label"></label>
+            <label class="col-md-2 col-form-label">직업</label>
+            <div class="col-md-2">
+              <b-form-select class="form-control" v-model="form.occupation" :options="$store.state.occupation" />
+            </div>
+          </div>
+          <div class="form-group row" v-if="form.mainField === 'finance'">
+            <label class="col-md-2 col-form-label">가족형태</label>
+            <div class="col-md-2">
+              <b-form-select class="form-control" v-model="form.familyType" :options="$store.state.familyType" />
+            </div>
+            <label class="col-md-2 col-form-label"></label>
+            <label class="col-md-2 col-form-label">관심사</label>
+            <div class="col-md-2">
+              <b-form-select class="form-control" v-model="form.interest" :options="$store.state.interest" />
+            </div>
+          </div>
+          <div class="form-group row" v-if="form.mainField === 'finance'">
+            <label class="col-md-2 col-form-label">월평균소득</label>
+            <div class="col-md-2">
+              <b-form-select class="form-control" v-model="form.monthlyIncome" :options="$store.state.monthlyIncome" />
+            </div>
+            <label class="col-md-2 col-form-label"></label>
+            <label class="col-md-2 col-form-label">보유자산</label>
+            <div class="col-md-2">
+              <b-form-select class="form-control" v-model="form.assets" :options="$store.state.assets" />
+            </div>
+          </div>
+          <div class="form-group row" v-if="form.mainField === 'finance'">
+              <label class="col-md-2 col-form-label">소득운용현황</label>
+              <div class="col-md-10 col-form-label">
+                <b-form-radio-group
+                  :options="$store.state.incomeManagement"
+                  checked="deposit"
+                  v-model="form.incomeManagement" @change="form.incomeManagement = ''">
+                </b-form-radio-group>
               </div>
             </div>
-            <div class="form-group row">
-              <label class="col-md-12 col-form-label" for="text-input"><strong>Ask a question...</strong></label>
+          <div class="form-group row">
+            <div class="col-md-12">
+              <textarea class="form-control" v-model="form.description" rows="9" placeholder="질문을 입력하세요."></textarea>
             </div>
-            <div class="from-group row">
-              <div class="col-md-12">
-                <b-form-input type="text" v-model="form.title" placeholder="제목을 입력하세요."></b-form-input>
-              </div>
+          </div>
+          <div class="form-group row">
+            <div class="col-md-12">
+              <vue-tags-input
+                v-model="tag"
+                :tags="tags"
+                @tags-changed="newTags => tags = newTags"
+                @before-adding-tag="addTagStyle"
+                style="max-width: 100%;"
+              />
             </div>
-            <br>
-            <div class="form-group row">
-              <label class="col-md-2 col-form-label">성별/나이</label>
-              <label class="col-md-2 col-form-label">{{ "male" === profile.gender ? "남" : "여"}} / {{ form.age }}</label>
-              <label class="col-md-2 col-form-label"></label>
-              <label class="col-md-2 col-form-label">직업</label>
-              <div class="col-md-2">
-                <b-form-select class="form-control" v-model="form.occupation" :options="$store.state.occupation" />
-              </div>
+          </div>
+          <div class="form-group row">
+            <div class="col-md-12 text-center">
+              <b-button class="button-comment" variant="success" type="submit" v-on:click="createQuestion">Post Your Question</b-button>
             </div>
-            <div class="form-group row" v-if="form.mainField === 'finance'">
-              <label class="col-md-2 col-form-label">가족형태</label>
-              <div class="col-md-2">
-                <b-form-select class="form-control" v-model="form.familyType" :options="$store.state.familyType" />
-              </div>
-              <label class="col-md-2 col-form-label"></label>
-              <label class="col-md-2 col-form-label">관심사</label>
-              <div class="col-md-2">
-                <b-form-select class="form-control" v-model="form.interest" :options="$store.state.interest" />
-              </div>
-            </div>
-            <div class="form-group row" v-if="form.mainField === 'finance'">
-              <label class="col-md-2 col-form-label">월평균소득</label>
-              <div class="col-md-2">
-                <b-form-select class="form-control" v-model="form.monthlyIncome" :options="$store.state.monthlyIncome" />
-              </div>
-              <label class="col-md-2 col-form-label"></label>
-              <label class="col-md-2 col-form-label">보유자산</label>
-              <div class="col-md-2">
-                <b-form-select class="form-control" v-model="form.assets" :options="$store.state.assets" />
-              </div>
-            </div>
-            <div class="form-group row" v-if="form.mainField === 'finance'">
-                <label class="col-md-2 col-form-label">소득운용현황</label>
-                <div class="col-md-10 col-form-label">
-                  <b-form-radio-group
-                    plain
-                    :options="$store.state.incomeManagement"
-                    checked="deposit"
-                    v-model="form.incomeManagement" @change="form.incomeManagement = ''">
-                  </b-form-radio-group>
-                </div>
-              </div>
-            <div class="form-group row">
-              <div class="col-md-12">
-                <textarea class="form-control" v-model="form.description" rows="9" placeholder="질문을 입력하세요."></textarea>
-              </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-md-12">
-                <vue-tags-input
-                  v-model="tag"
-                  :tags="tags"
-                  @tags-changed="newTags => tags = newTags"
-                  @before-adding-tag="addTagStyle"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer text-sm-right">
-          <button class="btn btn-sm btn-primary" type="submit" v-on:click="createQuestion">
-            Post Your Question
-          </button>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
