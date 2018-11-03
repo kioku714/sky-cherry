3d0be5d10aab3d60dd95e7e43469d8ccdc135c24
유미령 <kioku714@gmail.com>
2018-11-03 16:50:34 +0900

    주석 수정.

--- backend/controllers/answer.controller.js
+++ backend/controllers/answer.controller.js
@@ -1,21 +1,11 @@
 var Answer = require('../models/answer.model');
 
 /**
- * Create new question
- * @property {string} req.body.title
+ * Create new answer
  * @property {string} req.body.description
- * @property {string} req.body.mainField
- * @property {string} req.body.subField
- * @property {Answer[]} req.body.answers
- * @property {like[]} req.body.likes
- * @property {string} req.body.occupation
- * @property {string} req.body.familyType
- * @property {string} req.body.interest
- * @property {string} req.body.monthlyIncome
- * @property {string} req.body.assets
- * @property {string} req.body.incomeManagement
- * @property {string[]} req.body.tags
- * @returns {Question}
+ * @property {ObjectId} req.body.createdBy
+ * @property {ObjectId} req.body.question
+ * @returns {Answer}
  */
 function create(req, res, next) {
     const answer = new Answer({
