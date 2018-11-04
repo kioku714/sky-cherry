b7575f7c1e856c5a2cfd42b1f62f44ffefce2c54
chihwan <clghks@gmail.com>
2018-11-04 14:40:21 +0900

    태그 로딩 #70 태그 검색 #71

--- frontend/src/views/Tags.vue
+++ frontend/src/views/Tags.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="animated fadeIn">
-    <h1>Tags</h1>
+    <h3>태그는 여러분들의 질문을 다른, 비슷한 내용의 질문들과 함께 분류하는 키워드입니다. 적절한 태그를 사용하면 여러분의 질문을 다른 사람들이 더 잘 찾아 답변할 수 있습니다. 또한 관심 있는 칼럼이나 리포트를 찾아볼 수 있습니다.</h3>
+    <b-form-input class="search-input" type="text" v-model="search" placeholder="Search for questions"></b-form-input>
+    <div class="tags-container">
+      <div class="question-tag" v-for="tag in filteredList" :key="tag">
+        {{ tag }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +17,14 @@ export default {
   },
   data () {
     return {
+      search: ''
+    }
+  },
+  computed: {
+    filteredList () {
+      return this.$store.state.tags.filter(tas => { 
+        return tas.toLowerCase().includes(this.search.toLowerCase())
+      })
     }
   },
   methods: {
