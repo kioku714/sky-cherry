32a5506f3ee13c1f81f10028663b2b4b9c4f8f94
유미령 <kioku714@gmail.com>
2018-11-11 00:36:05 +0900

    컬럼에서 사용하던 css 삭제.

--- frontend/src/views/Column.vue
+++ frontend/src/views/Column.vue
@@ -4,33 +4,33 @@
       <b-row>
         <!-- Style -->
         <b-col sm="6">
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="12">
-              <span class="title"><u><b>Style</b></u></span>
+              <span class="pr-2"><u><b>Style</b></u></span>
               <span><small>More</small></span>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/style_01.jpg">
+              <b-img src="/static/img/column/style_01.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>롱패딩, 어디까지 비교해봤니? 브랜드 롱패딩 총정리!</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/style_02.jpg">
+              <b-img src="/static/img/column/style_02.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>스웨터와 잘 어울리는 귀걸이엔 어떤 것이 있을까</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/style_03.jpg">
+              <b-img src="/static/img/column/style_03.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>요즘 대세 어글리 슈즈, 편안함과 스타일 모두 Get!</div>
@@ -40,33 +40,33 @@
         </b-col>
         <!-- Place -->
         <b-col sm="6">
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="12">
-              <span class="title"><u><b>Place</b></u></span>
+              <span class="pr-2"><u><b>Place</b></u></span>
               <span><small>More</small></span>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/place_01.jpg">
+              <b-img src="/static/img/column/place_01.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>망원동 따끈따끈한 신상 카페·맛집 리스트</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/place_02.jpg">
+              <b-img src="/static/img/column/place_02.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>아직도 못 봤니? 핑크뮬리 앞에서 인생샷 찍자</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/place_03.jpg">
+              <b-img src="/static/img/column/place_03.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>떠나요 경주로, 황리단길 핫플레이스</div>
@@ -78,33 +78,33 @@
       <b-row>
         <!-- Culture -->
         <b-col sm="6">
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="12">
-              <span class="title"><u><b>Culture</b></u></span>
+              <span class="pr-2"><u><b>Culture</b></u></span>
               <span><small>More</small></span>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/culture_01.jpg">
+              <b-img src="/static/img/column/culture_01.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>뮤지컬 ‘위대한 쇼맨’, 영화랑 비교하기</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/culture_02.jpg">
+              <b-img src="/static/img/column/culture_02.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>다가오는 겨울, 따뜻한 커피와 카페에서 읽기 좋은 책</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/culture_03.jpg">
+              <b-img src="/static/img/column/culture_03.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>쇼미더머니777 요즘 힙한 레이블 분석해봤어요</div>
@@ -114,33 +114,33 @@
         </b-col>
         <!-- Finance -->
         <b-col sm="6">
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="12">
-              <span class="title"><u><b>Finance</b></u></span>
+              <span class="pr-2"><u><b>Finance</b></u></span>
               <span><small>More</small></span>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/finance_01.jpg">
+              <b-img src="/static/img/column/finance_01.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>美 금리 인상·韓 금리 동결, 어떤 영향 있을까</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/finance_02.jpg">
+              <b-img src="/static/img/column/finance_02.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>P2P 투자, 너도 나도 따라하면 안되는 이유</div>
               <div><small>2018-10-29</small></div>
             </b-col>
           </b-row>
-          <b-row>
+          <b-row class="py-2">
             <b-col sm="4" cols="6">
-              <img src="/static/img/column/finance_03.jpg">
+              <b-img src="/static/img/column/finance_03.jpg" fluid class="m-1"/>
             </b-col>
             <b-col sm="8" cols="6">
               <div>‘금알못’ 사회초년생의 CMA 통장 개설기</div>
