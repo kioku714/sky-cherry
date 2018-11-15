b7fb4ca59bc083ead1fbd2610721a3fcf87fed18
유미령 <kioku714@gmail.com>
2018-11-16 03:35:05 +0900

    질문 등록 화면이 모바일 해상도에서도 제대로 나오도록 css 수정.

--- frontend/src/views/NewQuestion.vue
+++ frontend/src/views/NewQuestion.vue
@@ -5,7 +5,7 @@
         <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
           <div class="form-group row">
             <label class="col-md-2 col-form-label">* 분야 : </label>
-            <div class="col-md-2">
+            <div class="col-md-2 mb-1">
               <b-form-select class="form-control" v-model="form.mainField">
                 <option v-for="option in $store.state.fieldItems" v-bind:value="option.mainFieldValue" :key="option.mainFieldValue">
                   {{ option.mainFieldName }}
@@ -31,39 +31,44 @@
             </div>
           </div>
           <br>
-          <div class="form-group row">
-            <label class="col-md-2 col-form-label">성별/나이</label>
-            <label class="col-md-2 col-form-label">{{ "male" === profile.gender ? "남" : "여" }} / {{ $moment({}).diff($moment(profile.birthday), 'years') }}</label>
-            <label class="col-md-2 col-form-label"></label>
-            <label class="col-md-2 col-form-label">직업</label>
-            <div class="col-md-2">
+          <b-row class="form-group">
+            <b-col sm="2" cols="4">
+              <label class="col-form-label">성별/나이:</label>
+            </b-col>
+            <b-col sm="3" cols="8">
+              <label class="col-form-label">{{ "male" === profile.gender ? "남" : "여" }} / {{ $moment({}).diff($moment(profile.birthday), 'years') }}</label>
+            </b-col>
+            <b-col sm="2" cols="4">
+              <label class="col-form-label">직업:</label>
+            </b-col>
+            <b-col sm="3" cols="8">
               <b-form-select class="form-control" v-model="form.occupation" :options="$store.state.occupation" />
-            </div>
-          </div>
+            </b-col>
+          </b-row>
           <div class="form-group row" v-if="form.mainField === 'finance'">
-            <label class="col-md-2 col-form-label">가족형태</label>
+            <label class="col-md-2 col-form-label">가족형태:</label>
             <div class="col-md-2">
               <b-form-select class="form-control" v-model="form.familyType" :options="$store.state.familyType" />
             </div>
             <label class="col-md-2 col-form-label"></label>
-            <label class="col-md-2 col-form-label">관심사</label>
+            <label class="col-md-2 col-form-label">관심사:</label>
             <div class="col-md-2">
               <b-form-select class="form-control" v-model="form.interest" :options="$store.state.interest" />
             </div>
           </div>
           <div class="form-group row" v-if="form.mainField === 'finance'">
-            <label class="col-md-2 col-form-label">월평균소득</label>
+            <label class="col-md-2 col-form-label">월평균소득:</label>
             <div class="col-md-2">
               <b-form-select class="form-control" v-model="form.monthlyIncome" :options="$store.state.monthlyIncome" />
             </div>
             <label class="col-md-2 col-form-label"></label>
-            <label class="col-md-2 col-form-label">보유자산</label>
+            <label class="col-md-2 col-form-label">보유자산:</label>
             <div class="col-md-2">
               <b-form-select class="form-control" v-model="form.assets" :options="$store.state.assets" />
             </div>
           </div>
           <div class="form-group row" v-if="form.mainField === 'finance'">
-              <label class="col-md-2 col-form-label">소득운용현황</label>
+              <label class="col-md-2 col-form-label">소득운용현황:</label>
               <div class="col-md-10 col-form-label">
                 <b-form-radio-group
                   :options="$store.state.incomeManagement"
