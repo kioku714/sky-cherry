844e7b37264ca950a4dcacaab492f709a69b0e8c
andy <rygh4775@gmail.com>
2018-11-16 23:39:07 +0900

    refact : 헤더에 로그인/로그아웃 아이콘 적용

--- frontend/src/views/Questions.vue
+++ frontend/src/views/Questions.vue
@@ -91,7 +91,6 @@ export default {
       this.distance = 0
       this.$http.get('/api/questions')
         .then((response) => {
-          console.log(response.data)
           this.questions = response.data
         })
         .then(() => {
